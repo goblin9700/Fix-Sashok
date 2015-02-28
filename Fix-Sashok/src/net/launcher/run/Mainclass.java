cbd8ead5d2088dbe166401030a154fdab92f053a
zenit <zenit@m.ru>
2015-03-01 01:01:37 +0300

    fix

--- Fix-Sashok/src/net/launcher/run/Mainclass.java
+++ Fix-Sashok/src/net/launcher/run/Mainclass.java
@@ -13,29 +13,34 @@ public class Mainclass
 {
 	public static void main(String[] args) throws Exception
 	{	
-		File dir = new File(BaseUtils.getAssetsDir().toString());
-		if(!dir.exists()) dir.mkdirs();
-		InputStream stream = Starter.class.getResourceAsStream("/net/launcher/theme/favicon.png");
-	    if (stream == null) {
-	    }
-	    OutputStream resStreamOut = null;
-	    int readBytes;
-	    byte[] buffer = new byte[4096];
-	    try {
-	        resStreamOut = new FileOutputStream(new File(BaseUtils.getAssetsDir().toString()+"/favicon.png"));
-	        while ((readBytes = stream.read(buffer)) > 0) {
-	            resStreamOut.write(buffer, 0, readBytes);
+		boolean test = (args.length != 0)?(args[0].equals("true")?true:false):false;
+        if(test) {
+			File dir = new File(BaseUtils.getAssetsDir().toString());
+			if(!dir.exists()) dir.mkdirs();
+			InputStream stream = Starter.class.getResourceAsStream("/net/launcher/theme/favicon.png");
+		    if (stream == null) {
+		    }
+		    OutputStream resStreamOut = null;
+		    int readBytes;
+		    byte[] buffer = new byte[4096];
+		    try {
+		        resStreamOut = new FileOutputStream(new File(BaseUtils.getAssetsDir().toString()+"/favicon.png"));
+		        while ((readBytes = stream.read(buffer)) > 0) {
+		            resStreamOut.write(buffer, 0, readBytes);
+		        }
+		    } catch (IOException e1) {
+		        e1.printStackTrace();
+		    } finally {
+		        stream.close();
+		        resStreamOut.close();
+		    }
+			Frame.start();
+			if(BaseUtils.getPropertyBoolean("Music", true))
+			{
+	           new MusPlay(Settings.iMusicname);
 	        }
-	    } catch (IOException e1) {
-	        e1.printStackTrace();
-	    } finally {
-	        stream.close();
-	        resStreamOut.close();
-	    }
-		Frame.start();
-		if(BaseUtils.getPropertyBoolean("Music", true))
-		{
-           new MusPlay(Settings.iMusicname);
+        } else {
+        	Starter.main(null);
         }
 	}
 }
